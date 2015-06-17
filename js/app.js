4ff53279b505858f1ec218f50fda9f25f311084d
Tom Milburn <thomasjosephmilburn@gmail.com>
2015-06-16 23:41:41 -0400

    starting to generate test svg files

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var width = 1600;
 var height = 500;
-var bound = 5;
+var bound = 0;
 
 function Point(x, y) {
   this.x = typeof x !== 'undefined' ? x : Math.random() * width;
@@ -19,8 +19,24 @@ function generatePoints(n) {
   return points;
 }
 
-var points = generatePoints(10);
-var boundingBox = {xl: bound, xr: width - bound, yt: bound, yb: height - bound};
-var voronoi = new Voronoi();
-var diagram = voronoi.compute(points, boundingBox);
-console.log(diagram);
+
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var points = generatePoints(10);
+  var boundingBox = {xl: bound, xr: width - bound, yt: bound, yb: height - bound};
+  var voronoi = new Voronoi();
+  var diagram = voronoi.compute(points, boundingBox);
+  // console.log(diagram);
+  var svgNS = "http://www.w3.org/2000/svg";  
+  var myPolygon = document.createElementNS(svgNS,"polygon"); //to create a circle. for rectangle use "rectangle"
+  var p = "";
+  for(var i = 0; i < diagram.cells[0].halfedges.length; i++){
+    var e = diagram.cells[0].halfedges[i].edge;
+    console.log(e.va, e.vb);
+    p += e.va.x + "," + e.va.y + " ";
+
+  }
+  myPolygon.setAttributeNS(null, "points", p);
+
+  document.getElementById("mySVG").appendChild(myPolygon);
+});
